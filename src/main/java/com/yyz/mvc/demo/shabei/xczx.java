781f4cce0354605658692af8c04c2b2051aa54e2
yyz <105575309@qq.com>
2022-09-10 15:50:58 +0800

    second commit

--- src/main/java/com/yyz/mvc/demo/shabei/xczx.java
+++ src/main/java/com/yyz/mvc/demo/shabei/xczx.java
@@ -3,5 +3,7 @@ package com.yyz.mvc.demo.shabei;
 public class xczx {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,world2");
+        System.out.println("hello,world3");
     }
 }
